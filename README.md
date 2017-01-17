d8c65c66602853831bfcc631957eb4b2a7ae6f10
Tony Spiro <tspiro@tonyspiro.com>
2017-01-17 14:54:56 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Creative Agency
+![Creative Agency](https://cosmicjs.com/uploads/d41050d0-d140-11e6-8fbd-bf2ca03de273-email-capture.jpg?w=1200)
+###[View Demo](https://cosmicjs.com/apps/creative-agency/demo)
+A website for a creative agency.  Includes team management, portfolio management and contact form (powered by MailGun).
+
+1. [Log in to Cosmic JS](https://cosmicjs.com).
+2. Create a Bucket.
+3. Go to Your Bucket > Apps.
+4. Install the [Creative Agency App](https://cosmicjs.com/apps/creative-agency).
+5. Edit the MailChimp List URL Metafield to point to your MailChimp List. (see below)
+6. Deploy your Email Capture App to the Cosmic App Server at Your Bucket > Web Hosting.
+
+###Getting Started
+```
+git clone https://github.com/cosmicjs/creative-agency
+cd creative-agency
+yarn
+```
+####Start app
+```
+yarn start
+```
+####Start app connected to your Cosmic JS Bucket
+```
+COSMIC_BUCKET=your-bucket-slug yarn start
+```
+Open [http://localhost:3000](http://localhost:3000).
+###Setting up MailGun
+1. Go to MailGun and login to your account or setup a new account
+2. Get your api key and domain
+3. Add your api key and domain to your environment valiables, or hard code them into `app.js` (not advised)
---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express')
+var app = express()
+var hogan = require('hogan-express')
+app.engine('html', hogan)
+app.set('port', (process.env.PORT || 3000))
+app.use('/', express.static(__dirname + '/public/'))
+var Cosmic = require('cosmicjs')
+var bucket_slug = process.env.COSMIC_BUCKET || 'creative-agency'
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.get('/', function(req, res) {
+  Cosmic.getObjects({ bucket: { slug: bucket_slug } }, function(err, response) {
+    var cosmic = response
+    res.locals.cosmic = cosmic
+    res.render('index.html')
+  })
+})
+app.post('/', function(req, res) {
+  var api_key = process.env.MAILGUN_KEY // add mailgun key
+  var domain = process.env.MAILGUN_DOMAIN // add mailgun domain
+  if (!api_key || !domain)
+    return res.end('You must add a MailGun api key and domain.  Contact your developer to add these values.');
+  var mailgun = require('mailgun-js')({ apiKey: api_key, domain: domain })
+  var message = 'Name: ' + req.body.name + '\n\n' +
+  'Subject: ' + req.body.subject + '\n\n' +
+  'Message: ' + req.body.message + '\n\n'
+  var data = {
+    from: 'Your Website <me@' + domain + '>',
+    to: process.env.RECEIVING_EMAIL || 'tony@cosmicjs.com',
+    subject: req.body.name + ' sent you a new message: ' + req.body.subject,
+    text: message
+  }
+  mailgun.messages().send(data, function (error, body) {
+    res.end('Success!  Message sent.') // Do some sort of redirect here with a success message
+  })
+})
+app.listen(app.get('port'))
